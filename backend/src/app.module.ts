c63ff4c6583969aa1f0300b6bcafcea796781be2
Supernova <jaskanwal196@gmail.com>
2025-01-13 00:35:06 +0900

    Config Service Backend

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: determineEnvFilePath(),
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
+
+function determineEnvFilePath(): string {
+  const environment = process.env.NODE_ENV || 'development';
+  return `.env.${environment}`;
+}
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,14 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService)
   // Enable CORS with specific options or defaults
   app.enableCors({
-    origin: process.env.CORS_ORIGIN ?? 'http://localhost:5173',  // Replace with your frontend's URL
+    origin: configService.get('CORS_ORIGIN') ?? 'http://localhost:5173',  // Replace with your frontend's URL
     credentials: true,                // If you're using cookies or other credentials
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
   });
-  await app.listen(process.env.PORT ?? 9000);
+  await app.listen(configService.get('PORT') ?? 9000);
 }
 bootstrap();
