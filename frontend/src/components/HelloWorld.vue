3c160ebbea8633e7e47ff8814ac569cb6afcec29
Supernova <jaskanwal196@gmail.com>
2025-01-12 22:17:48 +0900

    Changing backend prod URL

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -7,7 +7,7 @@ const apiUrl = import.meta.env.VITE_API_URL
 
 const fetchMessage = async () => {
   try {
-    const response = await axios.get(`${apiUrl}`)
+    const response = await axios.get(`${apiUrl}/api`)
     msg.value = response.data.message
   } catch (error) {
     console.error('Error fetching message:', error)
