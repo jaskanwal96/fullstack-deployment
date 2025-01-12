3c874959f3877ba57f8a95bc1c4c069f07ed93b3
Supernova <jaskanwal196@gmail.com>
2025-01-13 00:20:20 +0900

    Prod Config Backend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // Enable CORS with specific options or defaults
   app.enableCors({
-    origin: 'http://localhost:5173',  // Replace with your frontend's URL
+    origin: process.env.CORS_ORIGIN ?? 'http://localhost:5173',  // Replace with your frontend's URL
     credentials: true,                // If you're using cookies or other credentials
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
   });
