d75bbcf37d43c77650ce9850aeb09f628ed76c24
Supernova <jaskanwal196@gmail.com>
2025-07-05 14:47:46 +0900

    Ingress Setup

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import * as os from 'os';
 
 @Controller()
 export class AppController {
@@ -8,5 +9,10 @@ export class AppController {
   @Get('api/message')
   getMessage() {
     return { message: this.appService.getMessage() };
+  }
+
+  @Get()
+  getPodHostname(): string {
+    return `Handled by pod: ${os.hostname()}`;
   }
 }
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,11 +4,11 @@ import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const configService = app.get(ConfigService)
+  const configService = app.get(ConfigService);
   // Enable CORS with specific options or defaults
   app.enableCors({
-    origin: configService.get('CORS_ORIGIN') ?? 'http://localhost:5173',  // Replace with your frontend's URL
-    credentials: true,                // If you're using cookies or other credentials
+    origin: configService.get('CORS_ORIGIN') ?? 'http://localhost:5173', // Replace with your frontend's URL
+    credentials: true, // If you're using cookies or other credentials
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
   });
   await app.listen(configService.get('PORT') ?? 9000);
